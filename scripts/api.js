0c924479dcb1f86b9ee45c206f7863d3a847ce39
jenseneducation-Clara-Berglund <54704837+jenseneducation-Clara-Berglund@users.noreply.github.com>
2019-11-11 22:39:01 +0100

    Complete api.js

--- scripts/api.js
+++ scripts/api.js
@@ -1,3 +1,20 @@
 /*
 This file contains only functionality for accessing the Flickr API
 */
+
+/* Function that takes in parameters "searchTerm" and "numResults". 
+The queryString contains the query-parameters that affects the result coming from the API. 
+The function returns a promise through the fetch-API which contains JSON-data if resolved or an error if rejected.
+*/
+
+const getImages = (searchTerm, numResults) => {
+  const queryString =
+    `?nojsoncallback=1` +
+    `&api_key=82034b3450cc7d27778963c1b8099e2b` +
+    `&method=flickr.photos.search` +
+    `&format=json` +
+    `&safe_search=1` +
+    `&text=${searchTerm}` +
+    `&per_page=${numResults}`;
+  return fetch("https://api.flickr.com/services/rest" + queryString);
+};
